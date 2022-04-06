a7dd78be907659dbefdf9a8d2df50a7eefec05ed
jonathanromero <jonathandr600@gmail.com>
2022-04-06 16:40:11 -0300

    Error solucionado

--- index.js
+++ index.js
@@ -2,20 +2,19 @@
   class Pizzas{
     constructor(id, nombre, precio)
     {
-      this.id = id
+      this.id = id,
       this.nombre = nombre,
       this.precio = precio
     }
   }
   function nuevaPizza()
   {
-    let idIngresado = parseInt(prompt("Ingrese el id de su pizza"))
+    let idIngresado = parseInt(prompt("Ingrese el id de su pizza"));
     let nombreIngresado = prompt("Ingrese el nombre de tu pizza");
     let precioIngresado = parseInt(prompt("Ingresa el precio de tu pizza"));
     let nuevaPizza = new Pizzas(idIngresado,nombreIngresado, precioIngresado);
     pizzeria.push(nuevaPizza)
   }
-  pizzeria.push(nuevaPizza);
   nuevaPizza();
   let productos = [
     {id:1, nombre:"Napolitana", precio: 1000},
@@ -23,11 +22,10 @@
     {id:3, nombre:"4 Quesos", precio: 1400},
     {id:4, nombre:"Pepperoni", precio: 1100},
     {id:5, nombre:"Jamon y Panceta", precio: 1300}
-
   ]
-  let nombreIngresado = prompt("Ingresa un tipo de Pizza...")
-  let productoEncontrado = productos.filter((producto)=>producto.nombre.indexOf(nombreIngresado)!==-1);
+
+  pizzeria = pizzeria.concat(productos);
+   // pizzeria.push(productos);
+  let nombreIngresado = prompt("Ingresa el tipo de pizza que esta buscando...");
+  let productoEncontrado = pizzeria.filter((producto)=>producto.nombre.indexOf(nombreIngresado)!==-1);
   console.log(productoEncontrado);
-  pizzeria.push(productos);
-  console.log(pizzeria);
-  // Hola Franco, intente juntar este codigo con el que esta comentado arriba... mi idea era que el usuario al crear su "pizza" con el .push pueda encontrarla en los arrays... pero no me deja (limpie un  poco el codigo). Lo que yo hice fue poner a pizzeria.push(productos) y borrar las arrays de arriba (napo, hawa) pero nada :/
